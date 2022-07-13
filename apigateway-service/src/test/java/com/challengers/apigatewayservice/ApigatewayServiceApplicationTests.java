e17790602e07f6d8fa640dd3f5054b5e38a65b86
ha-jinwoo <gkwlsdn95@naver.com>
2022-07-13 15:08:14 +0900

    feat : apigateway 초기 설정

---
+++ apigateway-service/src/test/java/com/challengers/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.challengers.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

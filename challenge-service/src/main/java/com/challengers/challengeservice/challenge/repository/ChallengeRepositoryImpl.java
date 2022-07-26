86fc928f4c7ba46b4e80086398a3fa574cd63d06
JunSeong <kjs35323@naver.com>
2022-07-26 13:07:29 +0900

    fix : 챌린지 검색시 종료된 챌린지는 검색되지 않게 변경

--- challenge-service/src/main/java/com/challengers/challengeservice/challenge/repository/ChallengeRepositoryImpl.java
+++ challenge-service/src/main/java/com/challengers/challengeservice/challenge/repository/ChallengeRepositoryImpl.java
@@ -5,52 +5,46 @@ import com.challengers.challengeservice.challenge.domain.Challenge;
 import com.challengers.challengeservice.challenge.domain.ChallengeStatus;
 import com.challengers.challengeservice.challenge.dto.ChallengeSearchCondition;
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.support.PageableExecutionUtils;
 
 import javax.persistence.EntityManager;
 
-import java.util.List;
 
 import static com.challengers.challengeservice.challenge.domain.QChallenge.*;
 import static com.challengers.challengeservice.challengetag.domain.QChallengeTag.*;
 import static com.challengers.challengeservice.tag.domain.QTag.*;
 import static org.springframework.util.StringUtils.hasText;
 
-public class ChallengeRepositoryImpl implements ChallengeRepositoryCustom {
+public class ChallengeRepositoryImpl extends Querydsl4RepositorySupport implements ChallengeRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
     public ChallengeRepositoryImpl(EntityManager em) {
+        super(Challenge.class);
         this.queryFactory = new JPAQueryFactory(em);
     }
 
     @Override
     public Page<Challenge> search(ChallengeSearchCondition condition, Pageable pageable) {
-        List<Challenge> result = queryFactory
-                .select(challenge).distinct()
-                .from(challenge)
-                .join(challenge.challengeTags.challengeTags, challengeTag)
-                .join(challengeTag.tag, tag)
-                .where(searchCond(condition)
-                        .and(challenge.status.in(ChallengeStatus.READY,ChallengeStatus.IN_PROGRESS)))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
+        return applyPagination(pageable, contentQuery -> contentQuery
+                        .select(challenge).distinct()
+                        .from(challenge)
+                        .join(challenge.challengeTags.challengeTags, challengeTag)
+                        .join(challengeTag.tag, tag)
+                        .where(searchCond(condition)
+                                .and(challenge.status.in(ChallengeStatus.READY, ChallengeStatus.IN_PROGRESS))),
 
-        JPAQuery<Long> countQuery = queryFactory
-                .select(challenge.count())
-                .from(challenge)
-                .where(searchCond(condition));
-
-        return PageableExecutionUtils.getPage(result,pageable,countQuery::fetchOne);
+                countQuery -> countQuery
+                        .select(challenge).distinct()
+                        .from(challenge)
+                        .join(challenge.challengeTags.challengeTags, challengeTag)
+                        .join(challengeTag.tag, tag)
+                        .where(searchCond(condition)
+                                .and(challenge.status.in(ChallengeStatus.READY, ChallengeStatus.IN_PROGRESS)))
+        );
     }
 
     private BooleanBuilder searchCond(ChallengeSearchCondition condition) {
